6a809fbdeb3551cd4b40261c2aa92f6941cb63fb
Bruno Selhorst <bselhorst88@gmail.com>
2024-10-01 07:56:00 -0500

    Java RESTful API criado para o bootcamp da Claro

---
+++ claro-bootcamp/src/main/java/me/dio/claro_bootcamp/ClaroBootcampApplication.java
@@ -0,0 +1,13 @@
+package me.dio.claro_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaroBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaroBootcampApplication.class, args);
+	}
+
+}
